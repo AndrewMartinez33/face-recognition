90d95890e0d1c61f72c262aae25fca9e504ad553
Andrew A. Martinez <andrew.a.martinez33@gmail.com>
2019-03-28 10:41:33 -0700

    changed fetch endpoint to heroku backend

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://git.heroku.com/rocky-beyond-40790/signin', {
+    fetch('https://rocky-beyond-40790.herokuapp.com/signin', {
       method: 'post',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
